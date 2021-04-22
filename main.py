714e1966b2d25f83876a60d56921274d5386d82c
Brajesh Mohapatra <76986598+AaruniTechSol-Brajesh@users.noreply.github.com>
2021-04-22 23:38:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -90,4 +90,5 @@ def attendance():
         return render_template('home.html')
 if __name__=="__main__":
     #app.run(host = '0.0.0.0', port = 8080)
-    app.run(debug = True)
+    #app.run(debug = True)
+    app.run()
