7274e03286c757b6b0be0313e2c9033e17deb916
Brajesh Mohapatra <76986598+AaruniTechSol-Brajesh@users.noreply.github.com>
2021-04-23 00:51:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,8 +63,8 @@ def attendance():
         cap = cv2.VideoCapture(0)
         while True:
             success, img = cap.read()
-            imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-            imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+            #imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+            imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             facesCurFrame = face_recognition.face_locations(imgS)
             encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
             for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
